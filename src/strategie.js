1cef157c90fa0bf4467dd52ec25b17b075741fb9
Loïc Knuchel <loicknuchel@gmail.com>
2015-12-01 01:29:49 +0100

    use coords

--- src/strategie.js
+++ src/strategie.js
@@ -10,18 +10,18 @@ var Geo = (function(){
     forEachInCircle: forEachInCircle
   };
 
-  function distance(x1, y1, x2, y2){
-    var x = x1-x2, y = y1-y2;
+  function distance(coord1, coord2){
+    var x = coord1[0]-coord2[0], y = coord1[1]-coord2[1];
     return Math.sqrt((x*x)+(y*y));
   }
-  function isInCircle(centerX, centerY, radius, pointX, pointY){
-    return distance(centerX, centerY, pointX, pointY) <= radius;
+  function isInCircle(centerCoord radius, pointCoord){
+    return distance(centerCoord, pointCoord) <= radius;
   }
-  function forEachInCircle(centerX, centerY, radius, iteratee){
-    for(var i=centerX-radius; i<centerX+radius; i++){
-      for(var j=centerY-radius; j<centerY+radius; j++){
-        if(isInCircle(centerX, centerY, radius, i, j)){
-          iteratee(i, j);
+  function forEachInCircle(centerCoord, radius, iteratee){
+    for(var i=centerCoord[0]-radius; i<centerCoord[0]+radius; i++){
+      for(var j=centerCoord[1]-radius; j<centerCoord[1]+radius; j++){
+        if(isInCircle(centerCoord, radius, [i, j])){
+          iteratee([i, j]);
         }
       }
     }
@@ -55,15 +55,15 @@ function safestPlace(coordinates, range, towers){
     // IMPROVE : choose towers with less power...
     var minFiringTowers = towers.length+1;
     var bestPlaces = [];
-    Geo.forEachInCircle(coordinates[0], coordinates[1], range, function(x, y){
+    Geo.forEachInCircle(coordinates, range, function(coord){
         var firingTowers = _.filter(towers, function(tower){
-            return Geo.isInCircle(tower.coordinates[0], tower.coordinates[1], tower.firing_range, x, y);
+            return Geo.isInCircle(tower.coordinates, tower.firing_range, coord);
         });
         if(firingTowers.length < minFiringTowers){
             minFiringTowers = firingTowers.length;
-            bestPlaces = [[x, y]];
+            bestPlaces = [coord];
         } else if(firingTowers.length === minFiringTowers){
-            bestPlaces.push([x, y]);
+            bestPlaces.push(coord);
         }
     });
     return bestPlaces[0];
@@ -72,14 +72,14 @@ function safestPlace(coordinates, range, towers){
 // return towers shooting at the coordinates
 function getShootingTowers(coordinates, towers){
     return _.filter(towers, function(tower){
-        return Geo.isInCircle(tower.coordinates[0], tower.coordinates[1], tower.firing_range, coordinates[0], coordinates[1]);
+        return Geo.isInCircle(tower.coordinates, tower.firing_range, coordinates);
     });
 }
 
 // sort items by distance to coordinates
 function sortItemsByDistance(coordinates, items){
     return _.sortBy(items, function(item){
-        return Geo.distance(coordinates[0], coordinates[1], item.coordinates[0], item.coordinates[1]);
+        return Geo.distance(coordinates, item.coordinates);
     });
 }
 
